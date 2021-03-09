77737f2a71f1f946fc0a94ae4f72957474e97d85
Grunder-96 <grunder-96@mail.ru>
2021-03-09 16:33:58 +0300

    25 %

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if (typeof(members) !== "array") {
+    return false;}
+    let arr = [];
+    for (let i = 0; i < members.length; i ++) {
+      arr.push(members[i][0]);
+    }
+    console.log(arr.toUpperCase().sort().join(""));
 };
+// let answ = [];
+// createDreamTeam = [
+//   'amelia',
+//   'ruby',
+//   'lily',
+//   'grace',
+//   'millie',
+//   'daisy',
+//   'freya',
+//   'erin',
+//   'megan',
+//   'jasmine',
+//   'brooke',
+// ];
+// for (let i = 0; i < createDreamTeam.length; i ++) {
+//   answ.push(createDreamTeam[i][0]);
+// }
+
+// console.log(answ.join("").toUpperCase());

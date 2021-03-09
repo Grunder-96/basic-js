867249adc24298a49954aa84da4fd96f235aadf5
Grigory Taran <grunder-96@mail.ru>
2021-03-09 19:00:19 +0300

    Complete!!!

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,30 +2,15 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
 
-  if (typeof(members) !== "array") {
+  if (typeof(members) !== "array" && typeof(members) !== "object" || !members) {
     return false;}
     let arr = [];
     for (let i = 0; i < members.length; i ++) {
-      arr.push(members[i][0]);
+      if (typeof(members[i]) !== "string") {
+        continue;
+      }
+       let k = members[i].trim();
+      arr.push(k[0].toUpperCase());
     }
-    console.log(arr.toUpperCase().sort().join(""));
+    return arr.sort().join("");
 };
-// let answ = [];
-// createDreamTeam = [
-//   'amelia',
-//   'ruby',
-//   'lily',
-//   'grace',
-//   'millie',
-//   'daisy',
-//   'freya',
-//   'erin',
-//   'megan',
-//   'jasmine',
-//   'brooke',
-// ];
-// for (let i = 0; i < createDreamTeam.length; i ++) {
-//   answ.push(createDreamTeam[i][0]);
-// }
-
-// console.log(answ.join("").toUpperCase());

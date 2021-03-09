79ddb8632c3b03f89addfa22d3389d90b208dfec
Grunder-96 <grunder-96@mail.ru>
2021-03-09 14:00:21 +0300

    complete

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,16 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
- if (typeof(sampleActivity) !== "string" || typeof(Number(sampleActivity)) == 'number' || sampleActivity <= 0 || sampleActivity >= MODERN_ACTIVITY) {
-   return false;
- } else {
-   k = 0.693 / HALF_LIFE_PERIOD;
-   return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (k));
- }
+
+module.exports = function dateSample(a) {
+  if (typeof a !== "string") return false;
+  const MODERN_ACTIVITY= 15; 
+  const HALF_LIFE_PERIOD= 5730;
+  const log = 0.693;
+  const k = log / HALF_LIFE_PERIOD;
+  
+
+
+  let b = parseFloat(a);
+  if (b <= 0) return false;
+  if (b >= MODERN_ACTIVITY) return false;
+  if (!isFinite(b)) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / b) / k);
+
 };
+
+
 
 
 
